a2a9cec4da757ceab0ca2a572988235f4c6ce10f
Tiago Gomes <tidgomes@gmail.com>
2026-03-30 10:23:51 -0300

    running the application

--- src/MyApp.java
+++ src/MyApp.java
@@ -1,2 +1,16 @@
 public class MyApp {
+
+    public void main(String[] args) {
+        TaskTracker taskTracker = new TaskTracker();
+//        taskTracker.addTask("explore cave");
+//        taskTracker.addTask("get some wood");
+//        taskTracker.addTask("build a house");
+//        taskTracker.addTask("find diamonds");
+
+
+        taskTracker.markDone("317ab0ce-bb50-455c-8648-6ba4930eb562");
+        taskTracker.quit();
+
+        taskTracker.listTasks("Done");
+    }
 }
